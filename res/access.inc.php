46e6966a3b2b8c34648a8217aa8d1d3ecd7a021f
MAURICIOP10 <mauricio.pinedo9309@alumnos.udg.mx>
2022-11-17 22:58:12 -0600

    creando mi primer sitio web
    creando sitio de calcetines personaliza2

---
+++ res/access.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 'wbp24n6s';
+$imSettings['access']['users'] = array(
+	'admin' => array(
+		'groups' => array('74nsbho3'),
+		'id' => '74nsbho3',
+		'name' => 'Admin',
+		'password' => '63n12pe4',
+		'email' => '',
+		'page' => 'index.html'
+	),
+	'nuevousuario' => array(
+		'groups' => array('m8msyuw3'),
+		'id' => 'm8msyuw3',
+		'name' => 'Nuevousuario',
+		'password' => 'nn46p407',
+		'email' => '',
+		'page' => 'index.html'
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('74nsbho3');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
